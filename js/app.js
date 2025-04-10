8487ac218015e313f3e687ef62368362551c2a59
Marcelo Marques Albuquerque <marcelo.marques.a98@gmail.com>
2025-04-10 14:54:23 -0300

    Adicionando um alerta ao devolver o jogo

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ function alterarStatus(id) {
     let botao = gameClicado.querySelector('.dashboard__item__button');
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
+        alert('Tem certeza que deseja devolver?');
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Alugar'; //trocando o texto do botão com o .textContent
