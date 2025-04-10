974d418ac4c3bb65a7356b571537eec15cef6ee7
Marcelo Marques Albuquerque <marcelo.marques.a98@gmail.com>
2025-04-10 14:18:22 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img'); //"querySelector==procure um elemento no HTML, não o texto, mas sim a TAG"
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar'; //trocando o texto do botão com o .textContent
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+
+}
